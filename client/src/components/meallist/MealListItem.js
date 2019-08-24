2f1c4a52870c3efe9dd77fd245ef5e8ef336bf87
Mathew Stein <mathew.stein97@gmail.com>
2019-08-24 14:23:04 +1000

    Minor changes to remove warnings

--- client/src/components/meallist/MealListItem.js
+++ client/src/components/meallist/MealListItem.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import { setCurrentMeal } from "../../redux/actions/mealActions";
 import { connect } from "react-redux";
