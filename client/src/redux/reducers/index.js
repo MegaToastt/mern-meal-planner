57e23fb3040403548b8d3b4ae9a2a784b5869117
Mathew Stein <mathew.stein97@gmail.com>
2019-08-27 19:17:08 +1000

    Added new alert reducer to root

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -1,5 +1,10 @@
 import { combineReducers } from "redux";
 import mealReducer from "./mealReducer";
 import authReducer from "./authReducer";
+import alertReducer from "./alertReducer";
 
-export default combineReducers({ meal: mealReducer, auth: authReducer });
+export default combineReducers({
+  meal: mealReducer,
+  auth: authReducer,
+  alerts: alertReducer
+});
