6e7181b6ae9b149db6612b3d65fda21d60a5235b
Mathew Stein <mathew.stein97@gmail.com>
2019-08-28 19:22:11 +1000

    Added floating label forms to edit form

--- client/src/components/meallist/MealListEditForm.js
+++ client/src/components/meallist/MealListEditForm.js
@@ -5,6 +5,7 @@ import PropTypes from "prop-types";
 import { editMeal } from "../../redux/actions/mealActions";
 import MealAddCheckbox from "./MealAddCheckbox";
 import Alertbox from "../Alertbox";
+import FloatingLabelInput from "./FloatingLabelInput";
 
 import PerfectScrollbar from "react-perfect-scrollbar";
 import "react-perfect-scrollbar/dist/css/styles.css";
@@ -49,17 +50,17 @@ const MealListEditForm = ({ isAuthenticated, editMeal, currentMeal }) => {
         {isAuthenticated && <Redirect to="/" />}
         <h2>Edit Meal</h2>
         <form onSubmit={handleSubmit} className="form">
-          <label htmlFor="name">Name</label>
-          <input
-            type="text"
+          <FloatingLabelInput
             name="name"
+            title="Name"
+            type="text"
             onChange={e => setName(e.target.value)}
             value={name}
           />
-          <label htmlFor="description">Description</label>
-          <input
-            type="text"
+          <FloatingLabelInput
             name="description"
+            title="Description"
+            type="text"
             onChange={e => setDescription(e.target.value)}
             value={description}
           />
@@ -67,35 +68,34 @@ const MealListEditForm = ({ isAuthenticated, editMeal, currentMeal }) => {
             ingredientList={ingredientList}
             addIngredient={addIngredient}
           />
-          <label htmlFor="calories">Calories</label>
-          <input
-            type="number"
+          <FloatingLabelInput
             name="calories"
+            type="number"
+            title="Calories"
             onChange={e => setCalories(e.target.value)}
             value={calories}
           />
-          <label htmlFor="protein">Protein</label>
-          <input
-            type="number"
+          <FloatingLabelInput
             name="protein"
+            type="number"
+            title="Protein"
             onChange={e => setProtein(e.target.value)}
             value={protein}
           />
-          <label htmlFor="fat">Fat</label>
-          <input
-            type="number"
+          <FloatingLabelInput
             name="fat"
+            type="number"
+            title="Fat"
             onChange={e => setFat(e.target.value)}
             value={fat}
           />
-          <label htmlFor="carbs">Carbs</label>
-          <input
-            type="number"
+          <FloatingLabelInput
             name="carbs"
+            type="number"
+            title="Carbs"
             onChange={e => setCarbs(e.target.value)}
             value={carbs}
           />
-          <input type="submit" value="Submit" />
         </form>
       </div>
     </PerfectScrollbar>
